c9f9e35e10489e96aa42f74b25cbb6ce5ef486ca
Ilya Katz <ilyakatz@gmail.com>
2018-08-25 21:09:51 -0700

    Listen to Ethereum events to get updates form the blockchain

--- Coursetro.sol
+++ Coursetro.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.4.18;
 contract Coursetro {
     string fName;
     uint age;
-    
+
+    event Instructor(
+        string name,
+        uint age
+    );
+
     function Coursetro() public {
     }
-    
+
     function setInstructor(string _name, uint _age) public {
         fName = _name;
         age = _age;
+        Instructor(fName, age);
     }
-    
+
     function getInstructor() public constant returns (string, uint) {
         return (fName, age);
     }
