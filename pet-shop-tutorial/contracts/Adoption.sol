c927fefc0e2f6134436ae338d6945aaee00fe009
Ilya Katz <ilyakatz@gmail.com>
2018-09-08 13:09:28 -0700

    Create a new contract method to allow addresses to give give pets back

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -1,8 +1,13 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.22;
 
 
 contract Adoption {
   address[16] public adopters;
+  address public owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
 
   // Adopting a pet
   function adopt(uint petId) public returns (uint) {
@@ -13,8 +18,19 @@ contract Adoption {
     return petId;
   }
 
+  function giveAway(uint petId) public {
+    require(petId >= 0 && petId <= 15);
+    require(adopters[petId] == msg.sender, "Can only give away pet owned by you");
+
+    adopters[petId] = owner;
+  }
+
   // Retrieving the adopters
   function getAdopters() public view returns (address[16]) {
     return adopters;
+  }
+
+  function getOwner() public view returns (address) {
+    return owner;
   }
 }
--- pet-shop-tutorial/test/TestAdoption.sol
+++ pet-shop-tutorial/test/TestAdoption.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.17;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
+import "./support/ThrowProxy.sol";
 
 contract TestAdoption {
   Adoption adoption = Adoption(DeployedAddresses.Adoption());
@@ -37,4 +38,35 @@ contract TestAdoption {
     Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
   }
 
+  function testGiveAwayPet() public {
+    address expected = adoption.getOwner() ;
+    adoption.adopt(8);
+    address a = adoption.adopters(8);
+
+    Assert.notEqual(a, expected, "Pet 8 should not be owned by shelter");
+
+    adoption.giveAway(8);
+    address adopter = adoption.adopters(8);
+    Assert.equal(adopter, expected, "Pet 8 should have been returned to shelter");
+  }
+
+  function testGiveAwayOnlyYourPet() public {
+    address expected = adoption.getOwner() ;
+    adoption.adopt(8);
+    adoption.giveAway(8);
+    address adopter = adoption.adopters(8);
+    Assert.equal(adopter, expected, "Pet 8 should have been returned to shelter");
+  }
+
+  function testGiveAwayPetThatIsNotYours() public {
+    Adoption thrower = Adoption(DeployedAddresses.Adoption());
+    ThrowProxy throwProxy = new ThrowProxy(address(thrower));
+
+    //prime the proxy.
+    Adoption(address(throwProxy)).giveAway(8);
+
+    bool r = throwProxy.execute.gas(200000)();
+
+    Assert.isFalse(r, "Should throw exception");
+  }
 }
